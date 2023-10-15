704669f72f9fb9d9ce8d9750538cefc54b5a03f7
Aetheridon <lewisvr@protonmail.com>
2023-10-15 22:51:33 +0100

    Basic frontend working, next is working on Camera selection.

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import json
 
-from flask import Flask
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def main_page():
-    return "<h1>Hello Kale<h1>"
+    if request.method == "POST":
+        API_KEY = request.form["api"]
+        print(request.form.get("Checkbox"))
+        print(API_KEY)
+        return render_template("index.html")
+    else:   
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
